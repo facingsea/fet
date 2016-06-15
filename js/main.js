5a3d68487a41d5070d1a8f3701b75aa12923abf4
facingsea <972695371@qq.com>
2016-06-15 23:36:28 +0800

    json format

--- js/main.js
+++ js/main.js
@@ -4,6 +4,28 @@
 
 $(function(){
     $("#json-format-common").click(function(){
-        //var
+        $('#processed-json').empty();
+        var json = $('#raw-json').val();
+        if(json == ''){
+            return;
+        }
+        var formatJson = null;
+        var error = null;
+        try{
+            formatJson = vkbeautify.json(json, 4);
+        }catch (e){
+            error = e.message;
+        }
+        if(formatJson != null){
+            var pre = $('<pre style="width: 100%; height: 90%;">'+formatJson+'</pre>');
+            $('#processed-json').append(pre);
+        }
+        if(error != null){
+            var pre = $('<pre style="color: red; font-weight: bold; width: 100%; height: 90%;">'+error+'</pre>');
+            $('#processed-json').append(pre);
+        }
     });
+    $('textarea.resizable:not(.processed)').TextAreaResizer();
+    $('div.resizable:not(.processed)').TextAreaResizer();
 });
+
