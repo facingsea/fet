4466e57d6a60b01e6b76f3be8ec88a56c6e73f21
facingsea <972695371@qq.com>
2016-06-17 15:18:46 +0800

    highlight format

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
  */
 
 $(function(){
+    //普通格式化
     $("#json-format-common").click(function(){
         $('#processed-json').empty();
         var json = $('#raw-json').val();
@@ -25,7 +26,46 @@ $(function(){
             $('#processed-json').append(pre);
         }
     });
+
+    //highlight格式化
+    $('#json-format-color').click(function(){
+        $('#processed-json').empty();
+        var json = $('#raw-json').val();
+        if(json == ''){
+            return;
+        }
+        var formatJson = null;
+        var error = null;
+        try{
+            formatJson = vkbeautify.json(json, 4);
+        }catch (e){
+            error = e.message;
+        }
+        if(formatJson != null){
+            var pre = $('<pre style=""><code class="json" id="code-content">'+formatJson+'</code></pre>');
+            $('#processed-json').append(pre);
+            $("#code-content").each(function(i, e){
+                console.log(e);
+                hljs.highlightBlock(e);
+            });
+        }
+        if(error != null){
+            var pre = $('<pre style="color: red; font-weight: bold;">'+error+'</pre>');
+            $('#processed-json').append(pre);
+        }
+    });
+
+    //处理textarea和div的调整大小
     $('textarea.resizable:not(.processed)').TextAreaResizer();
     $('div.resizable:not(.processed)').TextAreaResizer();
+
+    //navbar事件
+    $('.dropdown-menu a').click(function(event){
+        var ele = event.target;
+        var tarId = $(ele).attr('tab-id');
+        $('.nav-tabs a[href="#'+tarId+'"]').click();
+    });
+
+
 });
 
